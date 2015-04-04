89a219e2ffaf3e47e7bc28ea54e79f96dc34f66a
Konstantin Chukhlomin <chuhlomin@gmail.com>
2015-04-04 12:07:51 +0300

    Create new event (extended)

--- spec/adminService.spec.js
+++ spec/adminService.spec.js
@@ -17,14 +17,36 @@ describe('Admin Service', function () {
     });
 
     it('can add an event to the system', function () {
-        adminService.addNewEvent("Test Title", new Date());
-        expect(adminService.showAllEvents()[0].title).toEqual("Test Title");
+        adminService.addNewEvent(
+          'New Mega Event', // title
+          '20.04', // date
+          '13:45', // time
+          [
+            'Mc. Jackson',
+            'Mick Jagger'
+          ], // list of singers
+          {
+              'vip': 300,
+              'seat': 200,
+              'enter': 100
+          } // prices
+        );
+
+        expect(adminService.showAllEvents()[0].title).toEqual('New Mega Event');
+        expect(adminService.showAllEvents()[0].date).toEqual('20.04');
+        expect(adminService.showAllEvents()[0].time).toEqual('13:45');
+        expect(adminService.showAllEvents()[0].singers).toEqual(['Mc. Jackson', 'Mick Jagger']);
+        expect(adminService.showAllEvents()[0].prices).toEqual({
+            'vip': 300,
+            'seat': 200,
+            'enter': 100
+        });
     });
 
     it('cannot add an event with empty Title to the system', function () {
         expect(function() {
             adminService.addNewEvent(undefined, new Date());
-        }).toThrow(new Error("Validation error: cannot add an event with an empty Title"));
+        }).toThrow(new Error('Validation error: cannot add an event with an empty title'));
         expect(_.isEmpty(adminService.showAllEvents())).toBeTruthy();
     });
 
--- src/lib/adminService.js
+++ src/lib/adminService.js
@@ -2,17 +2,37 @@ var _ = require('underscore');
 
 function AdminService() {
     this.events = [];
+    this.requiredFields = ['title', 'date', 'time', 'singers', 'prices'];
 }
 
 AdminService.prototype = {
     events: null,
-    addNewEvent: function (title, date) {
-        if (_.isEmpty(title)) {
-            throw new Error("Validation error: cannot add an event with an empty Title");
-        }
-        this.events.push({title: title, date: date});
+    requiredFields: null,
+
+    addNewEvent: function (title, date, time, singers, prices) {
+        var event = {
+            title: title,
+            date: date,
+            time: time,
+            singers: singers,
+            prices: prices
+        };
+
+        this._validate(event);
+
+        this.events.push(event);
         //console.log("Successfully added new event!!!");
     },
+
+    _validate: function(event) {
+        var key;
+        for (key in this.requiredFields) {
+            if (!(event[this.requiredFields[key]])) {
+                throw new Error('Validation error: cannot add an event with an empty ' + this.requiredFields[key]);
+            }
+        }
+    },
+
     showAllEvents: function () {
         return _.map(this.events, function (event) {
             return _.clone(event);
