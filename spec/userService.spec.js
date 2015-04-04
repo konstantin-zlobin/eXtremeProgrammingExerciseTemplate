7380727fa834856c6bb86936e58694d17de74ff2
Eugene Afonin <eugene_afonin@epam.com>
2015-04-04 13:23:29 +0300

    find by all criteria

---
+++ spec/userService.spec.js
@@ -0,0 +1,54 @@
+var _ = require('underscore');
+
+describe('Admin Service', function () {
+    var AdminServiceConstructor;
+    var adminService;
+
+    beforeAll(function () {
+        AdminServiceConstructor = require('../src/lib/adminService');
+    });
+
+    beforeEach(function () {
+        adminService = new AdminServiceConstructor();
+    });
+
+    afterEach(function () {
+
+    });
+
+    it('can add an event to the system', function () {
+        adminService.addNewEvent("Test Title", new Date(2015, 5, 1, 19, 0),
+                                 ["Bono", "2Pac"], [1000, 500, 100]);
+        expect(adminService.showAllEvents()[0].title).toEqual("Test Title");
+        expect(adminService.showAllEvents()[0].date).toEqual(new Date(2015, 5, 1, 19, 0));
+        expect(adminService.showAllEvents()[0].stars).toEqual(["Bono", "2Pac"]);
+        expect(adminService.showAllEvents()[0].price).toEqual([1000, 500, 100]);
+    });
+
+    it('cannot add an event with empty Title to the system', function () {
+        expect(function() {
+            adminService.addNewEvent("", new Date(2015, 5, 1, 19, 0),
+                                     ["Bono", "2Pac"], [1000, 500, 100]);
+        }).toThrow(new Error("Validation error: cannot add an event with an empty Title"));
+        expect(_.isEmpty(adminService.showAllEvents())).toBeTruthy();
+    });
+
+    it('cannot add an event with current Date to the system', function () {
+        expect(function() {
+            adminService.addNewEvent("Test Title", new Date(),
+                                     ["Bono", "2Pac"], [1000, 500, 100]);
+        }).toThrow(
+            new Error("Validation error: cannot add an event with today date or in the past"));
+        expect(_.isEmpty(adminService.showAllEvents())).toBeTruthy();
+    });
+
+    it('cannot add an event with past Date to the system', function () {
+        expect(function() {
+            adminService.addNewEvent("Test Title", new Date(null),
+                                     ["Bono", "2Pac"], [1000, 500, 100]);
+        }).toThrow(
+            new Error("Validation error: cannot add an event with today date or in the past"));
+        expect(_.isEmpty(adminService.showAllEvents())).toBeTruthy();
+    });
+
+});
---
+++ src/lib/userService.js
@@ -0,0 +1,34 @@
+var _ = require('underscore');
+
+function AdminService() {
+    this.events = [];
+}
+
+AdminService.prototype = {
+    events: null,
+    addNewEvent: function (title, date, stars, price) {
+        if (_.isEmpty(title)) {
+            throw new Error("Validation error: cannot add an event with an empty Title");
+        }
+        dateNow = new Date();
+        dateNow.setHours(23);
+        dateNow.setMinutes(60);
+        if (date < dateNow) {
+            throw new Error("Validation error: cannot add an event with today date or in the past");
+        }
+        this.events.push({title: title,
+                          date: date,
+                          stars: stars,
+                          price: price});
+        //console.log("Successfully added new event!!!");
+    },
+    showAllEvents: function () {
+        return _.map(this.events, function (event) {
+            return _.clone(event);
+        });
+    }
+};
+
+
+
+module.exports = AdminService;
