24262bb8d82980f311cb4990dcb1c06e29fb6ef4
Eugene Afonin <eugene_afonin@epam.com>
2015-04-04 12:24:42 +0300

    Basic validation

--- spec/adminService.spec.js
+++ spec/adminService.spec.js
@@ -17,15 +17,27 @@ describe('Admin Service', function () {
     });
 
     it('can add an event to the system', function () {
-        adminService.addNewEvent("Test Title", new Date());
+        var date = new Date();
+        adminService.addNewEvent("Test Title", date, ["Prince", "ALisa", "Bricks"], 3.5, 2.99, 1.29);
         expect(adminService.showAllEvents()[0].title).toEqual("Test Title");
+        expect(adminService.showAllEvents()[0].date).toEqual(date);
+        expect(adminService.showAllEvents()[0].performers).toEqual(["Prince", "ALisa", "Bricks"]);
+        expect(adminService.showAllEvents()[0].ticketPriceVip).toEqual(3.5);
+        expect(adminService.showAllEvents()[0].ticketPriceTables).toEqual(2.99);
+        expect(adminService.showAllEvents()[0].ticketPriceEntrance).toEqual(1.29);
     });
 
-    it('cannot add an event with empty Title to the system', function () {
-        expect(function() {
-            adminService.addNewEvent(undefined, new Date());
-        }).toThrow(new Error("Validation error: cannot add an event with an empty Title"));
-        expect(_.isEmpty(adminService.showAllEvents())).toBeTruthy();
+        it('cannot add an event with empty Title to the system', function () {
+            expect(function() {
+                adminService.addNewEvent(undefined, new Date());
+            }).toThrow(new Error("Validation error: title, performers, ticketPriceVip, ticketPriceTables, ticketPriceEntrance can not be empty!"));
+            expect(_.isEmpty(adminService.showAllEvents())).toBeTruthy();
+        });
+        it('cannot add an event with just Title to the system', function () {
+            expect(function() {
+                adminService.addNewEvent('MegaConcert!');
+            }).toThrow(new Error("Validation error: date, performers, ticketPriceVip, ticketPriceTables, ticketPriceEntrance can not be empty!"));
+            expect(_.isEmpty(adminService.showAllEvents())).toBeTruthy();
     });
-
 });
+
--- src/lib/adminService.js
+++ src/lib/adminService.js
@@ -4,13 +4,37 @@ function AdminService() {
     this.events = [];
 }
 
+function validate(title, date, performers, ticketPriceVip, ticketPriceTables, ticketPriceEntrance) {
+    var invalidFields = [];
+
+    if (title == null) {
+        invalidFields.push('title');
+    }
+    if (date == null) {
+        invalidFields.push('date');
+    }
+    if (performers == null || performers.length == 0) {
+        invalidFields.push('performers');
+    }
+    if (ticketPriceVip == null) {
+        invalidFields.push('ticketPriceVip');
+    }
+    if (ticketPriceTables == null) {
+        invalidFields.push('ticketPriceTables');
+    }
+    if (ticketPriceEntrance == null) {
+        invalidFields.push('ticketPriceEntrance');
+    }
+    if(invalidFields.length > 0) {
+       throw new Error("Validation error: " + invalidFields.join(', ') + " can not be empty!");
+    }
+}
 AdminService.prototype = {
     events: null,
-    addNewEvent: function (title, date) {
-        if (_.isEmpty(title)) {
-            throw new Error("Validation error: cannot add an event with an empty Title");
-        }
-        this.events.push({title: title, date: date});
+    addNewEvent: function (title, date, performers, ticketPriceVip, ticketPriceTables, ticketPriceEntrance) {
+        validate(title, date, performers, ticketPriceVip, ticketPriceTables, ticketPriceEntrance);
+        this.events.push({title: title, date: date, performers: performers,
+                             ticketPriceVip: ticketPriceVip, ticketPriceTables: ticketPriceTables, ticketPriceEntrance: ticketPriceEntrance});
         //console.log("Successfully added new event!!!");
     },
     showAllEvents: function () {
